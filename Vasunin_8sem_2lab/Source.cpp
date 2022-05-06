4ae3c72d9ff4949a2b0b7dc94f85610f3d0c45d3
Nesterov Vlad <nesterovvlad100@gmail.com>
2022-05-06 10:41:39 +0300

    Добавьте файлы проекта.

---
+++ Vasunin_8sem_2lab/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int n = 0, m = 0;
+
+int main() {
+
+
+	cout << "Tipe in number of colors" << endl;
+	cin >> n;
+	cout << "Tipe in number of braslets" << endl;
+	cin >> m;
+	cout << pow(m, n);
+	return 1;
+}
